a9ea15f55b8566a101f9b88346d3a5ca375d78df
harclemadscam <joeapplegate1993@gmail.com>
2020-06-08 19:21:33 +0000

    Added route to team selection page

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/select-team')
+def team_select():
+    return render_template("team_select.html")
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
