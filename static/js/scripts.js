1eb78df0b4e3ae51aae96b6757369760127eb493
harclemadscam <joeapplegate1993@gmail.com>
2020-07-11 19:42:49 +0000

    Created js file, added for loop for position label text content

---
+++ static/js/scripts.js
@@ -0,0 +1,69 @@
+const positionLabels = document.getElementsByClassName("position-label");
+for (let i = 0; i < positionLabels.length; i++) {
+    if (positionLabels[i].textContent === "left-forward") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "left-centre-forward") {
+        positionLabels[i].textContent = "LCF"
+    }
+    if (positionLabels[i].textContent === "centre-forward") {
+        positionLabels[i].textContent = "CF"
+    }
+    if (positionLabels[i].textContent === "right-centre-forward") {
+        positionLabels[i].textContent = "RCF"
+    }
+    if (positionLabels[i].textContent === "left-attacking-midfield") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "attacking-midfield") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "right-attacking-midfield") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "left-midfield") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "left-centre-midfield") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "right-centre-midfield") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "right-midfield") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "left-wing-back") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "centre-defensive-midfield") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "right-wing-back") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "left-back") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "back-three-left") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "left-centre-back") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "centre-back") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "right-centre-back") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "back-three-right") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "right-back") {
+        positionLabels[i].textContent = "LF"
+    }
+    if (positionLabels[i].textContent === "goalkeeper") {
+        positionLabels[i].textContent = "GK"
+    }
+};
