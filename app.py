b9b68e292469ee192ecc1f94f7e54b811c2ad045
harclemadscam <joeapplegate1993@gmail.com>
2020-07-12 19:24:38 +0000

    Switched off flask debug mode in app.py

--- app.py
+++ app.py
@@ -336,4 +336,4 @@ def update_free_agent(player_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
